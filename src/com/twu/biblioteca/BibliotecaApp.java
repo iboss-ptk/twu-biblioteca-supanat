c982c0dc2b54807ba8f2cc9aa1124648fdb6d4a0
ibosz <supanat.ptk@gmail.com>
2016-08-10 09:52:24 +0700

    Add welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        System.out.println("Welcome to Biblioteca!");
     }
 }
